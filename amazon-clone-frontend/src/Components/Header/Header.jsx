c61e1bde74f0d7778dd7e95c2e04116ff79b45ad
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2025-11-05 13:05:31 +0100

    Clean up the old files

---
+++ amazon-clone-frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return (
+        <section>
+            <section>
+                <div>
+                    {/*log */}
+                    <a href="">
+                        <img src="https://pngimg.com/uploads/amazon/amazon_PNG13.png" alt="" />
+                    </a>
+                </div>
+            </section> 
+        </section>
+    )
+}
